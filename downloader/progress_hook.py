15ea0e2fbdaaed36f91d82845275686c970282c5
Mr Malik <121479735+mrmalikoffl@users.noreply.github.com>
2025-04-21 09:32:07 +0800

    Delete downloader directory

--- downloader/progress_hook.py
+++
@@ -1,5 +0,0 @@
-async def progress_hook(d, message):
-    """Send progress updates."""
-    if d["status"] == "downloading":
-        percent = d.get("_percent_str", "0%").strip()
-        await message.reply_text(f"⏬ Downloading: {percent}")
