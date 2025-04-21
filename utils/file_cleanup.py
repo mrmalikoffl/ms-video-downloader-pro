273700c07079bfd9e7a0081189a3186a66c5ca2f
Mr Malik <121479735+mrmalikoffl@users.noreply.github.com>
2025-04-21 09:32:57 +0800

    Delete utils directory

--- utils/file_cleanup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def cleanup_file(filename: str):
-    """Delete the file if it exists."""
-    if filename and os.path.exists(filename):
-        try:
-            os.remove(filename)
-        except Exception as e:
-            print(f"Failed to delete file {filename}: {e}")
